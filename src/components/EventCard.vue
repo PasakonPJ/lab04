97947c7aa25255f24e64e8cf17e596ae2baadee7
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-21 01:41:32 +0700

    1.10

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,5 +1,6 @@
 <template>
-  <router-link to="{name: 'EventDetails'}, params: {id:event.id}}">
+  <router-link class="event-link"
+  to="{name: 'EventDetails'}, params: {id:event.id}}">
     <div class="event-card">
       <span>@{{ event.time }} on {{ event.date }}</span>
       <h4>{{ event.title }}</h4>
@@ -31,5 +32,9 @@ export default {
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link{
+  color: #0964c0;
+  text-decoration: none;
 }
 </style>
--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -11,8 +11,7 @@ export default {
   props: ['id'],
   data() {
     return {
-      event: null,
-      
+      event: null
     }
   },
   created() {
