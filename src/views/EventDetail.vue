999d96b9838f60881917c57b0d841affd632210e
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-21 00:53:00 +0700

    until 1.5

--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -1,19 +1,27 @@
 <template>
-    <div>
-        <h1>{{event.title}}</h1>
-        <p>{{event.time}} on {{event.data}} @ {{event.location}}</p>
-        <p>{{event.description}}</p>
-    </div>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.data }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            event: null
-        }
-    },
-    created(){
-        // fetch event (by id) and set local event data
-    }        
+  data() {
+    return {
+      event: null,
+      id: 123
+    }
+  },
+  created() {
+    // fetch event (by id) and set local event data
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 }
 </script>
