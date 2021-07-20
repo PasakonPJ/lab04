eaa4258c3dda0a0ef956378f89349be70bd56e1f
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-21 00:30:08 +0700

    1.1

---
+++ src/views/EventDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{event.title}}</h1>
+        <p>{{event.time}} on {{event.data}} @ {{event.location}}</p>
+        <p>{{event.description}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            event: null
+        }
+    },
+    created(){
+        // fetch event (by id) and set local event data
+    }        
+}
+</script>
