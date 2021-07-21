336dff36062b47649e2e4bb26530b6201c1b253d
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-21 15:48:56 +0700

    1.11

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{EventList}">Home</router-link> |
+    <router-link :to="{About}">About</router-link>
   </div>
 
   <!-- new element -->
